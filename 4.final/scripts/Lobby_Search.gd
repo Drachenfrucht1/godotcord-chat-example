f369ed2310574adee5937cc056b4c2b478439b8c
Drachenfrucht1 <dominik.sucker@t-online.de>
2021-04-14 13:10:25 +0200

    Add lobby search

--- 4.final/scripts/Lobby_Search.gd
+++ 4.final/scripts/Lobby_Search.gd
@@ -2,13 +2,22 @@ extends Node2D
 
 func _ready():
 	GodotcordLobbyManager.connect("search_lobbies_callback", self, "_lobby_results");
-	var param;
+	var param = [];
 	GodotcordLobbyManager.search_lobbies(param, 10);
 
-func _lobby_results(lobbies):
-	for l in lobbies:
-		var lobby_scene = preload("res://Lobby_Search_Result.tscn");
-		#lobby_scene.$VBoxContainer/name.text = 
+func _lobby_results(lobby: GodotcordLobby):
+	var lobby_scene = preload("res://Lobby_Search_Result.tscn").instance();
+	lobby_scene.get_node("VBoxContainer/name").text = String(lobby.id);
+	lobby_scene.get_node("VBoxContainer/owner").text = "Owner: " + String(lobby.owner_id);
+	lobby_scene.get_node("secret").text = lobby.secret;
+	
+	lobby_scene.get_node("join").connect("pressed", self, "_on_join", [String(lobby.id), String(lobby.secret)]);
+	
+	$ScrollContainer/VBoxContainer.add_child(lobby_scene);
+
+func _on_join(id, secret):
+	NetworkGlobal.connect_to_lobby(id, secret);
+	
 
 func _on_back_pressed():
 	SceneSwitcher.change_scene_instant("res://Start_Menu.tscn");
